dc4ff1993ea02cfccfbf56e0751156ff99caf66a
Vitor.Lopes <vitoraugustoa@hotmail.com>
2019-06-14 14:14:35 -0300

    Adicionando o campo status no itemList

--- MyMoney-app/Front-end/mymoney/src/billingCycle/billingCycleForm.jsx
+++ MyMoney-app/Front-end/mymoney/src/billingCycle/billingCycleForm.jsx
@@ -5,12 +5,12 @@ import { bindActionCreators } from 'redux';
 
 import LabelAndInput from '../common/form/labelAndInput';
 import { init } from './redux/billingCycleActions';
-import CreditList from '../credit/creditList';
+import ItemList from '../itemList/itemList';
 
 class billingCycleForm extends Component {
 
     render() {
-        const { handleSubmit, buttonValue, readOnly, submitClass, credits } = this.props;
+        const { handleSubmit, buttonValue, readOnly, submitClass, credits, debts } = this.props;
         
         return (
             <form role="form" onSubmit={handleSubmit} >
@@ -19,7 +19,8 @@ class billingCycleForm extends Component {
                     <Field name="name" component={LabelAndInput} label="Nome" cols="12 4" placeholder="Informe o nome" readOnly={readOnly} />
                     <Field name="month" component={LabelAndInput} label="Mês" cols="12 4" placeholder="Informe o mês" readOnly={readOnly} />
                     <Field name="year" component={LabelAndInput} label="Ano" cols="12 4" placeholder="Informe o ano" readOnly={readOnly} />
-                    <CreditList cols="12 6" list={credits} readOnly={readOnly} />
+                    <ItemList cols="12 6" list={credits} readOnly={readOnly} field='credits' legend='Créditos'/>
+                    <ItemList cols="12 6" list={credits} readOnly={readOnly} field='debts' legend='Créditos'/>
                 </div>
                 <div className="box-footer">
                     <button type="submit" className={`btn btn-${submitClass}`}>{buttonValue}</button>
@@ -35,6 +36,7 @@ const selector = formValueSelector('billingCycleForm'); // Pegando informação 
 
 const mapStateToProps = state => ({
   credits: selector(state,'credits'), // buscando o atributo do formulário controlado pelo redux-form
+  debts: selector(state, 'debts'),
 });
 const mapDispatchToProps = dispatch => bindActionCreators({
     init 
